37bc00623d92f0319d6ca3923e1da5f44f5a15f2
shivang-16 <shivangyadav121@gmail.com>
2024-11-08 23:27:51 +0530

    initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }/
+
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js,jsx,ts,tsx}"];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
