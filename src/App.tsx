dc748f65a9eedca5c64398953ec392f497f37dac
Niranjani-sharma <Niranjanisharma30@gmail.com>
2024-11-10 18:20:07 +0530

    Added the footer, Fixed NavBar and added the Mission pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import './App.css'
-;
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from './pages/Home';
 import { About } from './pages/About';
@@ -9,27 +8,28 @@ import { Vision } from './pages/Vision';
 import { Mission } from './pages/Mission';
 import { Contact } from './pages/ContactUs';
 import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
 function App() {
-
   return (
     <Router>
-      <div>
-      <Navbar />
-        <Routes>
-          
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About />} />
-          <Route path="/coordinationcomittee" element={<CoordinatonComittee />} />
-          <Route path="/studentcomittee" element={<StudentComittee/>} />
-          <Route path="/vision" element={<Vision />} />
-          <Route path="/mission" element={<Mission />} />
-          <Route path="/contactus" element={<Contact />} />
-
-        </Routes>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About />} />
+            <Route path="/coordinationcommittee" element={<CoordinatonComittee />} />
+            <Route path="/studentcommittee" element={<StudentComittee/>} />
+            <Route path="/vision" element={<Vision />} />
+            <Route path="/mission" element={<Mission />} />
+            <Route path="/contactus" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
